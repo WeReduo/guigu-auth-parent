50e90312660be45b09f000683d00d5305fb0da37
sgm <2289424711@qq.com>
2023-11-30 09:53:24 +0800

    mapper类

---
+++ service-system/src/main/java/com/ataguigu/system/mapper/SysRoleMapper.java
@@ -0,0 +1,13 @@
+package com.ataguigu.system.mapper;
+
+import com.atguigu.model.system.SysRole;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.springframework.stereotype.Repository;
+
+import javax.annotation.Resource;
+
+@Repository
+public interface SysRoleMapper extends BaseMapper<SysRole> {
+
+
+}
