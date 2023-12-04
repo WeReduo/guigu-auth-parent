1a0e1b3e05cf62a0effb0278fa2730c1fd652d36
sgm <2289424711@qq.com>
2023-12-04 09:59:16 +0800

    增加了条件删除

--- service-system/src/test/java/com/ataguigu/system/test/SysRoleMappperTest.java
+++ service-system/src/test/java/com/ataguigu/system/test/SysRoleMappperTest.java
@@ -2,10 +2,12 @@ package com.ataguigu.system.test;
 
 import com.atguigu.model.system.SysRole;
 import com.ataguigu.system.mapper.SysRoleMapper;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.annotation.Resource;
+import java.util.Arrays;
 import java.util.List;
 
 @SpringBootTest
@@ -24,7 +26,7 @@ public class SysRoleMappperTest {
             System.out.println("sysRole = " + sysRole);
         }
     }
-
+//添加
     @Test
     public void testInsert(){
         SysRole sysRole = new SysRole();
@@ -36,4 +38,38 @@ public class SysRoleMappperTest {
         System.out.println(result); //影响的行数
         System.out.println(sysRole.getId()); //id自动回填
     }
+//修改
+    @Test
+    public void testUpdateById(){
+        SysRole sysRole=sysRoleMapper.selectById(1);
+        sysRole.setDescription("系统管理员");
+        int result = sysRoleMapper.updateById(sysRole);
+        System.out.println(result);
+
+
+    }
+    //批量删除
+    @Test
+    public void testDeleteBatchIds() {
+        int result = sysRoleMapper.deleteBatchIds(Arrays.asList(1, 2));
+        System.out.println(result);
+    }
+
+    @Test
+    public void testQueryWrapper() {
+        QueryWrapper<SysRole> queryWrapper = new QueryWrapper<>();
+        //条件查询
+//        queryWrapper.ge("role_code", "role");
+
+        //模糊查询
+        queryWrapper.like("role_name","用户管理员");
+        List<SysRole> users = sysRoleMapper.selectList(queryWrapper);
+        System.out.println(users);
+    }
+    @Test
+    public void  TestDel(){
+        QueryWrapper<SysRole> queryWrapper = new QueryWrapper<>();
+        queryWrapper.ge("role_code", "用户管理员");
+    }
+
 }
