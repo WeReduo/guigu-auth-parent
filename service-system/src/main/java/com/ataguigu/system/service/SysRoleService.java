ac427b15def115c3851ae4ba3362cecf279c8643
sgm <2289424711@qq.com>
2023-12-07 09:35:12 +0800

    SysRoleService

---
+++ service-system/src/main/java/com/ataguigu/system/service/SysRoleService.java
@@ -0,0 +1,12 @@
+package com.ataguigu.system.service;
+
+
+import com.atguigu.model.system.SysRole;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+import java.util.List;
+
+public interface SysRoleService extends IService<SysRole> {
+
+
+}
