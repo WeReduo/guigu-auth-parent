d588c5ed90d30e2f580cfd5a60e4a5e23cb10525
sgm <2289424711@qq.com>
2023-12-07 23:51:48 +0800

    全局异常处理类

--- common/service-util/src/main/java/com/ataguigu/system/config/exception/GlobalExceptionHandler.java
+++ common/service-util/src/main/java/com/ataguigu/system/config/exception/GlobalExceptionHandler.java
@@ -12,16 +12,23 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
+//    @ExceptionHandler(Exception.class)
+//    @ResponseBody
+//    public Result error(Exception e){
+//        e.printStackTrace();
+//
+//        return Result.fail().message("全局异常执行。。。");
+//    }
     @ExceptionHandler(Exception.class)
     @ResponseBody
-    public Result error(Exception e){
-        e.printStackTrace();
-
-        return Result.fail().message("执行了特定异常处理");
-    }
     public Result error(ArithmeticException e) {
         e.printStackTrace();
-        return Result.fail().message("执行了特定异常处理");
+        return Result.fail().message("执行特定异常。。。");
     }
-
+    @ExceptionHandler(GuiguException.class)
+    @ResponseBody
+    public Result error(GuiguException e2) {
+        e2.printStackTrace();
+        return Result.fail().code(e2.getCode()).message(e2.getMessage());
+    }
 }
