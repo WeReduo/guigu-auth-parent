1de43bc3223ff3f8dc22de6df7ee8358558f1d53
sgm <2289424711@qq.com>
2023-12-07 17:15:52 +0800

    加了Swagger之后的 SysRoleController

--- service-system/src/main/java/com/ataguigu/system/controller/SysRoleController.java
+++ service-system/src/main/java/com/ataguigu/system/controller/SysRoleController.java
@@ -2,22 +2,35 @@ package com.ataguigu.system.controller;
 
 import com.ataguigu.system.service.SysRoleService;
 import com.atguigu.model.system.SysRole;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+@Api("角色管理的接口")
 @RestController
 @RequestMapping("/admin/system/sysRole")
 public class SysRoleController {
     @Autowired
     private SysRoleService sysRoleService;
+    //http://localhost:8800/admin/system/sysRole/findAll
+    //查询所有记录
+    @ApiOperation("查询所有记录的接口")
     @GetMapping("/findAll")
     public List<SysRole> findAll() {
         List<SysRole> roleList = sysRoleService.list();
         return roleList;
+    }
+    //逻辑删除
+    @ApiOperation("逻辑删除的接口")
+    @DeleteMapping("/remove/{id}")
+    public  boolean removeRole(@PathVariable long id){
+        //调用方法进行删除
+        boolean isSucess=sysRoleService.removeById(id);
+
+        return isSucess;
+
     }
 
 }
