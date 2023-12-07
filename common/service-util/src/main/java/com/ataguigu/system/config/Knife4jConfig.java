b4d50840b51c7f37a5a68155871860eec9093abb
sgm <2289424711@qq.com>
2023-12-07 18:55:47 +0800

    knife4j配置信息

--- common/service-util/src/main/java/com/ataguigu/system/config/Knife4jConfig.java
+++ common/service-util/src/main/java/com/ataguigu/system/config/Knife4jConfig.java
@@ -1,4 +1,4 @@
-package com.ataguigu.system.com.atguigu.system.config;
+package com.ataguigu.system.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
