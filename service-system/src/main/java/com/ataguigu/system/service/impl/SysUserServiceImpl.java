6c9174add664b3e520dd3068c5765d8c4e3392c5
smg <2289424711@qq.com>
2023-12-28 12:04:54 +0800

    代码生成器 User

---
+++ service-system/src/main/java/com/ataguigu/system/service/impl/SysUserServiceImpl.java
@@ -0,0 +1,21 @@
+package com.ataguigu.system.service.impl;
+
+
+import com.ataguigu.model.system.SysUser;
+import com.ataguigu.system.mapper.SysUserMapper;
+import com.ataguigu.system.service.SysUserService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ * 用户表 服务实现类
+ * </p>
+ *
+ * @author shgm
+ * @since 2023-12-28
+ */
+@Service
+public class SysUserServiceImpl extends ServiceImpl<SysUserMapper, SysUser> implements SysUserService {
+
+}
