4d2d9c958fa3cc63c0a83fcfd7462ea3be00d083
smg <2289424711@qq.com>
2023-12-28 12:04:28 +0800

    代码生成器 User

---
+++ service-system/src/main/java/com/ataguigu/system/mapper/SysUserMapper.java
@@ -0,0 +1,17 @@
+package com.ataguigu.system.mapper;
+
+
+import com.ataguigu.model.system.SysUser;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 用户表 Mapper 接口
+ * </p>
+ *
+ * @author shgm
+ * @since 2023-12-28
+ */
+public interface SysUserMapper extends BaseMapper<SysUser> {
+
+}
