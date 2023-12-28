63ce1eb6d71fc25e1e6b8c58111c2e085646c646
smg <2289424711@qq.com>
2023-12-28 12:05:28 +0800

    代码生成器 User

---
+++ service-system/src/main/java/com/ataguigu/system/service/SysUserService.java
@@ -0,0 +1,17 @@
+package com.ataguigu.system.service;
+
+
+import com.ataguigu.model.system.SysUser;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ * 用户表 服务类
+ * </p>
+ *
+ * @author shgm
+ * @since 2023-12-28
+ */
+public interface SysUserService extends IService<SysUser> {
+
+}
