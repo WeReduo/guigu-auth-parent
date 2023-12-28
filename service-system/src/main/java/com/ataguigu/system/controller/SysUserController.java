91a5b4f860aac94103b60887f1cf3e7be5ca9cee
smg <2289424711@qq.com>
2023-12-28 12:04:08 +0800

    代码生成器 User

---
+++ service-system/src/main/java/com/ataguigu/system/controller/SysUserController.java
@@ -0,0 +1,23 @@
+package com.ataguigu.system.controller;
+
+
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ * 用户表 前端控制器
+ * </p>
+ *
+ * @author shgm
+ * @since 2023-12-28
+ */
+@Api("用户管理的接口")
+@RestController
+@RequestMapping("/admin/system/sysUser")
+public class SysUserController {
+
+}
+
