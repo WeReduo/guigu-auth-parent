9731f6fe77ee7ccab175bf44dbfeff39a2484dd0
smg <2289424711@qq.com>
2023-12-29 07:47:47 +0800

    SysUserRoleMapper

---
+++ service-system/src/main/java/com/ataguigu/system/mapper/SysUserRoleMapper.java
@@ -0,0 +1,13 @@
+package com.ataguigu.system.mapper;
+
+
+import com.ataguigu.model.system.SysUserRole;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@Mapper
+public interface SysUserRoleMapper extends BaseMapper<SysUserRole> {
+
+}
