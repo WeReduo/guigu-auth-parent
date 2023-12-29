49d5970534c173fb62cf9980d579536c1a321549
smg <2289424711@qq.com>
2023-12-29 19:50:38 +0800

    菜单表 Mapper 接口

---
+++ service-system/src/main/java/com/ataguigu/system/mapper/SysMenuMapper.java
@@ -0,0 +1,17 @@
+package com.ataguigu.system.mapper;
+
+
+import com.ataguigu.model.system.SysMenu;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 菜单表 Mapper 接口
+ * </p>
+ *
+ * @author shgm
+ * @since 2023-12-29
+ */
+public interface SysMenuMapper extends BaseMapper<SysMenu> {
+
+}
