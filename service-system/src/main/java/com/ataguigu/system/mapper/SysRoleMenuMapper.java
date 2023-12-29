7ab1bf0fe40cbde1ff878a6473872ded74556c47
smg <2289424711@qq.com>
2023-12-29 19:51:12 +0800

    SysRoleMenuMapper

---
+++ service-system/src/main/java/com/ataguigu/system/mapper/SysRoleMenuMapper.java
@@ -0,0 +1,14 @@
+package com.ataguigu.system.mapper;
+
+
+
+import com.ataguigu.model.system.SysRoleMenu;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@Mapper
+public interface SysRoleMenuMapper extends BaseMapper<SysRoleMenu> {
+
+}
